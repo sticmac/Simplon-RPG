67ef97dbf3d8902dad5933240e87e75f7329b7d7
Julien Lemaire <julien.lemaire@sticmac.fr>
2025-06-10 10:50:11 +0200

    Intégration de l'input manager

---
+++ Assets/Scripts/Player/PlayerControllerInputManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerControllerInputManager : MonoBehaviour
+{
+    [SerializeField] private CharacterMovement _movement;
+    [SerializeField] private PlayerAttack _attack;
+
+    void Update()
+    {
+        // Mouvement
+        //// 1. Version « bas niveau »
+        // Si j'appuie sur la flèche de droite, je vais à droite (+1)
+        // Si j'appuie sur la flèche de gauche, je vais à gauche (-1)
+        // Réciproque avec WASD : D va à droite (+1) et A va à gauche (-1)
+        /*bool right = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        bool left = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        float horizontal = 0f;
+        if (right)
+        {
+            horizontal = 1f;
+        }
+        else if (left)
+        {
+            horizontal = -1f;
+        }
+        _movement.ModifyMovement(horizontal);*/
+
+
+        //// 2. Version plus « haut niveau »
+        float horizontal = Input.GetAxis("Horizontal");
+        _movement.ModifyMovement(horizontal);
+
+
+        // Attaque
+        //// 1. Version « bas niveau »
+        //bool clicked = Input.GetMouseButtonDown(0);
+
+        //// 2. Version plus « haut niveau »
+        bool clicked = Input.GetButtonDown("Fire1");
+
+        if (clicked)
+        {
+            _attack.Attack();
+        }
+
+    }
+}
